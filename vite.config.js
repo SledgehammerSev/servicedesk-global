66b2d2228ab94877f111ed7238160b7d73905815
John Manoukian <john.manoukian@harrison.ai>
2026-03-16 16:13:24 +1100

    servicedesk app

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/servicedesk-global/'
+  base: '/hai-it-support/'
 })
+```
+
+After that, your deploy command will be:
+```
+npm run build
+npx gh-pages -d dist
